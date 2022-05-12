32da7dd845782615fb97bbd447f6c615041bc9d3
LuciDevilCoder <lucidevilcoder@gmail.com>
2022-05-13 00:38:51 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/todo.html");
+})
+
+app.listen(2000, function(){
+  console.log("Server started on port 2000!!!");
+});
